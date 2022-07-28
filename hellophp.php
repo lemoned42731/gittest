ab9a986b55a641f882ce0d4e0d49fca966e1b613
shigeta <shigeta@tmng.co.jp>
2022-07-28 16:02:44 +0900

    7/28_04

--- hellophp.php
+++ hellophp.php
@@ -4,16 +4,23 @@ $text = <<<EOM
 
 github練習用
 
+//コマンド早見表
+https://qiita.com/kohga/items/dccf135b0af395f69144
+
 //現在のブランチの確認
 git branch
 
 //ステージングの確認
 git status
 
-//コマンド早見表
-https://qiita.com/kohga/items/dccf135b0af395f69144
+//コミット状況の確認
+git reflog
 
-//
+//ステージング取り消し(前回のコミット位置まで戻る)
+git reset -q HEAD
+-qは出力しない
+git reset
+上記と同様(デフォルトでHEADつき)
 
 EOM;
 
