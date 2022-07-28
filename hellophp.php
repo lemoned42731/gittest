a248e38b8edf108a8d6b1c685342560eb607b186
shigeta <shigeta@tmng.co.jp>
2022-07-28 14:59:49 +0900

    7/28_01commit

--- hellophp.php
+++ hellophp.php
@@ -1,8 +1,14 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>mod</p>'; ?> 
- </body>
-</html>
+<?php
+
+$text = <<<EOM
+
+github練習用
+
+//現在のブランチの確認
+git branch
+
+7/28 ローカルからの更新
+
+EOM;
+
+echo $text;
